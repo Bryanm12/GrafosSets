17a2d6a91e196eb2fc6eddff7edf2e93129c2d16
Bryan <bryanm31421@gmail.com>
2024-07-03 23:19:55 -0500

    Grafos-Sets ejercicio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+import java.lang.classfile.components.ClassPrinter.Node;
+import java.util.List;
 public class App {
+   
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Graph<String> redSocial = new Graph<>();
+
+        NodeGraph<String> user = new NodeGraph<>("Mateo");
+        NodeGraph<String> user1 = new NodeGraph<>("Ana");
+        NodeGraph<String> user2 = new NodeGraph<>("Alicia");
+        NodeGraph<String> user3 = new NodeGraph<>("Pedro");
+
+        redSocial.addNode(user);
+        redSocial.addNode(user1);
+        redSocial.addNode(user2);
+        redSocial.addNode(user3);
+
+        redSocial.addConnection(user, user1);
+        redSocial.addConnection(user, user2);
+        redSocial.addConnection(user1, user2);
+        redSocial.addConnection(user1, user3);
+        List<NodeGraph<String>> friendsOfUser = redSocial.getFriends(user);
+        List<NodeGraph<String>> recommendFriendsUser = redSocial.recommendFriends(user);
+
+        System.out.println("Amigos de Mateo: ");
+        for(NodeGraph<String> friend : friendsOfUser){
+            System.out.println("- "+friend.getUser());
+        }
+
+        System.out.println("Amigos recomendados para Mateo: ");
+        for(NodeGraph<String> recommendFriend: recommendFriendsUser){
+            System.out.println("- "+recommendFriend.getUser());
+        }
     }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,18 +1,36 @@
 import java.util.List;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.*;
 public class Graph<T> {
-    private List<NodeGraph<T>> nodes;
+    private Map<NodeGraph<T>, List<NodeGraph<T>>> nodes;
 
+    public Graph(){
+        this.nodes= new HashMap<>();
+    }
     public void addNode(NodeGraph<T> node){
+        nodes.putIfAbsent(node,new ArrayList<>());
         
     }
     public void addConnection(NodeGraph<T> node1, NodeGraph<T> node2){
-
+        nodes.get(node1).add(node2);
+        nodes.get(node2).add(node1);
     }
-    public List<T> getFriends(NodeGraph<T> node){
-
+    public List<NodeGraph<T>> getFriends(NodeGraph<T> node){
+        return nodes.get(node);
     }
-    public List<T> recommendFriends(NodeGraph<T> node){
+    public List<NodeGraph<T>> recommendFriends(NodeGraph<T> node){
+        Set<NodeGraph<T>> friends = new HashSet<>(nodes.get(node));
+        Set <NodeGraph<T>> recommendFriends = new HashSet<>();
 
+        for(NodeGraph<T> friend : friends){
+            for(NodeGraph<T> friendOfFriend: nodes.get(friend)){
+                if(!friends.contains(friendOfFriend)&&!friendOfFriend.equals(node)){
+                    recommendFriends.add(friendOfFriend);
+                }
+            }
+        }
+
+        return new ArrayList<>(recommendFriends);
     }
 }
